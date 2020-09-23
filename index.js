91c53d140b6ac19326f8ebfd5a3e0215906839d5
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-09-23 21:33:48 +0200

    Bot now shows errors in the console
    This solves issue #44

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const {Client} = require('discord.js');
 const client = new Client();
 const {token} = require('./config.json');
 
+process.on('unhandledRejection', error => {
+    console.error('Unhandled promise rejection:', error);
+});
+
+client.on('error', error => {
+    console.error('The websocket connection encountered an error:', error);
+});
+
 client.login(token);

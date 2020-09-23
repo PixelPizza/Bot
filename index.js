2f5d7840b5a0992a1b8b7a2a7a9df6632bfef623
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-09-24 01:21:39 +0200

    log gets sent to pixel pizza on new guild
    This solves issue #55

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const {Client} = require('discord.js');
-const client = new Client();
-const {token} = require('./config.json');
-const {updateMemberSize,updateGuildAmount} = require('./functions');
+const {Client}=require('discord.js');
+const client=new Client();
+const {token}=require('./config.json');
+const {green}=require('./colors.json');
+const {updateMemberSize,updateGuildAmount,sendGuildLog,createEmbed}=require('./functions');
 
 process.on('unhandledRejection', error => {
     console.error('Unhandled promise rejection:', error);
@@ -15,6 +16,11 @@ client.on('ready', () => {
     updateGuildAmount(client);
     updateMemberSize(client);
     console.log("Pixel Pizza is ready");
+});
+
+client.on('guildCreate', guild => {
+    updateGuildAmount(client);
+    sendGuildLog(guild.name, guild.iconURL(), createEmbed(green, "Added", null, null, `${client.user.username} has been added to the guild ${guild.name}`, null, [], null, true, {text: guild.id}));
 });
 
 client.login(token);

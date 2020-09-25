f823c74f1e9c108d67c5aeebaa5a49d00a350ea6
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-09-25 20:26:56 +0200

    Getting executable commands
    This solves issue #96

--- commands/help.js
+++ commands/help.js
@@ -13,5 +13,32 @@ module.exports = {
     execute(message, args, client){
         const embedMsg = createEmbed(blue, null, null, {name: message.author.username, icon: message.author.displayAvatarURL()}, null, message.author.displayAvatarURL(), [], null, true, {text: client.user.username, icon: client.user.displayAvatarURL()});
         const embedMsgDM = createEmbed(blue, `**${this.name}**`, null, {name: message.author.username, icon: message.author.displayAvatarURL()}, null, message.author.displayAvatarURL(), [], null, true, {text: client.user.username, icon: client.user.displayAvatarURL()});
+        
+        const {commands, worker, teacher, staff, director} = message.client;
+        let workerCommands = commands.filter(command => command.userType == "worker");
+        let teacherCommands = commands.filter(command => command.userType == "teacher");
+        let staffCommands = commands.filter(command => command.userType == "staff");
+        let directorCommands = commands.filter(command => command.userType == "director");
+        let executableCommands = commands.filter(command => command.userType == "all");
+        if (worker){
+            workerCommands.each(command => {
+                executableCommands.set(command.name, command);
+            });
+        }
+        if (teacher){
+            teacherCommands.each(command => {
+                executableCommands.set(command.name, command);
+            });
+        }
+        if (staff){
+            staffCommands.each(command => {
+                executableCommands.set(command.name, command);
+            });
+        }
+        if (director){
+            directorCommands.each(command => {
+                executableCommands.set(command.name, command);
+            });
+        }
     }
 }

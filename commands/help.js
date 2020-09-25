1db382998251e938a629b5e7594d9f66bf7c6dd1
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-09-25 21:00:53 +0200

    bot now sends executable commands
    This solves issue #97

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,6 @@
 const {createEmbed}=require('../functions');
 const {blue}=require('../colors.json');
+const {prefix}=require('../config.json');
 
 module.exports = {
     name: "help",
@@ -11,33 +12,40 @@ module.exports = {
     cooldown: 5,
     userType: "all",
     execute(message, args, client){
-        const embedMsg = createEmbed(blue, null, null, {name: message.author.username, icon: message.author.displayAvatarURL()}, null, message.author.displayAvatarURL(), [], null, true, {text: client.user.username, icon: client.user.displayAvatarURL()});
-        const embedMsgDM = createEmbed(blue, `**${this.name}**`, null, {name: message.author.username, icon: message.author.displayAvatarURL()}, null, message.author.displayAvatarURL(), [], null, true, {text: client.user.username, icon: client.user.displayAvatarURL()});
-        
-        const {commands, worker, teacher, staff, director} = message.client;
-        let workerCommands = commands.filter(command => command.userType == "worker");
-        let teacherCommands = commands.filter(command => command.userType == "teacher");
-        let staffCommands = commands.filter(command => command.userType == "staff");
-        let directorCommands = commands.filter(command => command.userType == "director");
+        const embedMsg=createEmbed(blue,null,null,{name:message.author.username,icon:message.author.displayAvatarURL()},null,message.author.displayAvatarURL(),[],null,true,{text:client.user.username,icon:client.user.displayAvatarURL()});
+        const embedMsgDM=createEmbed(blue,`**${this.name}**`,null,{name:message.author.username,icon:message.author.displayAvatarURL()},null,message.author.displayAvatarURL(),[],null,true,{text:client.user.username,icon:client.user.displayAvatarURL()});
+        let {commands,worker,teacher,staff,director}=message.client;
         let executableCommands = commands.filter(command => command.userType == "all");
         if (worker){
-            workerCommands.each(command => {
+            commands.filter(command=>command.userType=="worker").each(command => {
                 executableCommands.set(command.name, command);
             });
         }
         if (teacher){
-            teacherCommands.each(command => {
+            commands.filter(command=>command.userType=="teacher").each(command => {
                 executableCommands.set(command.name, command);
             });
         }
         if (staff){
-            staffCommands.each(command => {
+            commands.filter(command=>command.userType=="staff").each(command => {
                 executableCommands.set(command.name, command);
             });
         }
         if (director){
-            directorCommands.each(command => {
+            commands.filter(command => command.userType == "director").each(command => {
                 executableCommands.set(command.name, command);
+            });
+        }
+        if (!args.length){
+            embedMsgDM.setDescription(`\nYou can send '${prefix}${this.name} ${this.usage}' to get help for specific commands`).addFields({name:'all commands',value:executableCommands.map(command=>command.name).join(', ')},{name:'Commands amount',value:executableCommands.size});
+            return message.author.send(embedMsgDM).then(()=>{
+                if(message.channel.type==="dm")return;
+                embedMsg.setDescription("I've sent you a DM with all commands");
+            }).catch(error=>{
+                console.error(`Could not send help DM to ${message.author.tag}.\n${error}`);
+                embedMsg.setColor(red).setDescription("I can't DM you. Do you have DMs disabled?");
+            }).then(()=>{
+                message.channel.send(embedMsg);
             });
         }
     }

4ef38fd0f2b96bcf5552511b7ef0a49b85fb65c1
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-09-27 23:35:33 +0200

    updated dbfunctions to reconnect on timezone error

--- dbfunctions.js
+++ dbfunctions.js
@@ -1,6 +1,6 @@
 const{default: Db}=require('mysql2-async');
 const{host,user,password,database}=require('./database.json');
 const db_config={host:host,user:user,password:password,database:database,skiptzfix:true};
-let db=new Db(db_config);setInterval(function(){db=new Db(db_config);}, 60000);
+let db;function handleError(){try{db=new Db(db_config);}catch(error){if(error.sqlMessage=="Unknown or incorrect time zone: 'UTC'"){setTimeout(handleError,100);}else{throw error;}}}handleError();
 exports.query=async(query,options=[])=>{return db.query(query,options);}
 exports.addUser=async(userId)=>{if(isNaN(userId)||userId.length!=18)return;const result=await this.query(`SELECT * FROM \`user\` WHERE userId = ?`,[userId]);if(result.length)return;this.query(`INSERT INTO \`user\`(userId) VALUES(?)`,[userId]);}

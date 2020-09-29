16fb73001666f452f6a1095710cffbfdca1ccdb0
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-09-30 00:10:35 +0200

    Fixed orders command
    This solves issue #9

--- commands/orders.js
+++ commands/orders.js
@@ -7,22 +7,25 @@ module.exports={
     name:"orders",
     description:"show all orders",
     minArgs:0,
-    maxArgs:1,
     usage:"<status>",
     cooldown:0,
     userType:"worker",
     pponly:false,
     async execute(message,args,client){
+        let status;
         let embedMsg=createEmbed(blue,`**${this.name}**`);
         let results;
+        if(args.length){
+            status=args.join(" ");
+        }
         if(!args.length){
             results=await query("SELECT * FROM `order` WHERE status NOT IN('deleted','delivered')");
-        } else if(!statuses.includes(args[0])){
-            embedMsg.setColor(red).setDescription(`${args[0]} is not a valid status`).addField("Statuses",statuses.join(", "));
+        } else if(!statuses.includes(status)){
+            embedMsg.setColor(red).setDescription(`${status} is not a valid status`).addField("Statuses",statuses.join(", "));
             if(!client.canSendEmbeds)embedMsg=`${embedMsg.description}\n\n${embedMsg.fields[0].name}\n${embedMsg.fields[0].value}`;
             return message.channel.send(embedMsg);
         } else {
-            results=await query("SELECT * FROM `order` WHERE status = ?",[args[0]]);
+            results=await query("SELECT * FROM `order` WHERE status = ?",[status]);
         }
         let ordersString=results.length?"\`":"no orders have been found";
         for(let i in results){

210774e0f7ab1715513fbe8154a8d2032f98f679
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-10-08 21:32:33 +0200

    Revert "test"
    This reverts commit 3a7a082c3720a2aa37f573d5e94c8abb75c6b4e1.

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
-import{WebhookClient,MessageEmbed}from'discord.js';
-import{botGuild,prefix,noiceboardMinValue}from'./config.json';
-import{voice,text}from"./channels.json";
-import{log}from'./webhooks.json';
-import{noice2}from'./emojis.json';
-import{noiceboard}from'./colors.json';
+const{WebhookClient,MessageEmbed}=require('discord.js');
+const{botGuild,prefix,noiceboardMinValue}=require('./config.json');
+const{voice,text}=require("./channels.json");
+const{log}=require('./webhooks.json');
+const{noice2}=require('./emojis.json');
+const{noiceboard}=require('./colors.json');
 exports.updateMemberSize=(client)=>{const guild=client.guilds.cache.get(botGuild);const allMembersChannel=client.channels.cache.get(voice.allMembers);const membersChannel=client.channels.cache.get(voice.members);const botsChannel=client.channels.cache.get(voice.bots);const members=guild.members.cache.filter(member=>!member.user.bot).size;allMembersChannel.setName(`All members: ${guild.memberCount}`);membersChannel.setName(`Members: ${members}`);botsChannel.setName(`Bots: ${guild.memberCount - members}`);}
 exports.updateGuildAmount=(client)=>{const suffixes=["k","m","b"];const activities=["PLAYING","STREAMING","LISTENING","WATCHING"];const activity=activities[Math.floor(Math.random()*activities.length)];const url="http://twitch.tv/";let serverAmout=client.guilds.cache.array().length;let suffixUsed="";for(let suffix in suffixes){if (serverAmout>1000){serverAmout/=1000;suffixUsed=suffix;} else break;}serverAmout=`${serverAmout}${suffixUsed}`;if (activity=="PLAYING"||activity=="STREAMING"){serverAmout=`with ${serverAmout}`;}client.user.setActivity(`${serverAmout} guilds | ${prefix}help`,{type:activity,url:url});}
 exports.sendGuildLog=(name,avatar,message)=>{const webhook=new WebhookClient(log.id,log.token);webhook.edit({name:name,avatar:avatar}).then(()=>{webhook.send(message);});}

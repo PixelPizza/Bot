67ba19d9f6feacbcf5ba9dfb2000af11022496d8
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-10-10 17:53:38 +0200

    Finished toggles command
    This is part of issue #129

---
+++ commands/toggles.js
@@ -0,0 +1,17 @@
+module.exports={
+    name:"toggles",
+    description:"shows all toggles",
+    args:false,
+    cooldown:60,
+    userType:"staff",
+    neededPerms:[],
+    pponly:true,
+    removeExp:false,
+    execute(message,args,client){
+        const toggles=[];
+        for(let toggle in client.toggles){
+            toggles.push(toggle);
+        }
+        message.channel.send(toggles.toString().replace(",","\n"));
+    }
+}

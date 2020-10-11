a649da636b5b5c6790b2ced6ff0d2eee7a9b1e75
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-10-11 13:57:14 +0200

    test

--- commands/levelset.js
+++ commands/levelset.js
@@ -34,7 +34,7 @@ module.exports={
             embedMsg.setDescription(`This user is not in Pixel Pizza`);
             return sendEmbed(embedMsg,message);
         }
-        console.log(isNan(amount));
+        console.log(isNaN(amount));
         setLevel(client,user.id,amount);
         embedMsg.setColor(blue).setDescription(`level ${amount} has been set for ${user.tag}`);
         sendEmbed(embedMsg,message);

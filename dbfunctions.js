8356703809bcb39ff48f34a05669bc8fc9575b21
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-10-27 22:46:12 +0100

    shortened dbfunctions file

--- dbfunctions.js
+++ dbfunctions.js
@@ -1,21 +1,161 @@
-const{default: Db}=require('mysql2-async');
-const{baseexp,addexp}=require('./level.json');
-const{host,user,password,database}=require('./database.json');
-const{botGuild,idLength}=require('./config.json');
-const{levelRoles}=require('./roles.json');
-const{addRole,removeRole,hasRole,randomInt}=require('./functions');
-const db_config={host:host,user:user,password:password,database:database,skiptzfix:true};
-let db;function handleError(){try{db=new Db(db_config);}catch(error){if(error.sqlMessage=="Unknown or incorrect time zone: 'UTC'"){setTimeout(handleError,100);}else{throw error;}}}handleError();
-const characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-exports.query=async(query,options=[])=>{return db.query(query,options);}
-exports.addUser=async(userId)=>{if(isNaN(userId)||userId.length!=18)return;const result=await this.query(`SELECT * FROM \`user\` WHERE userId = ?`,[userId]);if(result.length)return;this.query(`INSERT INTO \`user\`(userId) VALUES(?)`,[userId]);}
-exports.addExp=async(client,userId,amount)=>{return new Promise(async resolve=>{amount=parseInt(amount);if(isNaN(userId)||userId.length!=18||isNaN(amount))return;const results=await this.query("SELECT `exp` FROM `user` WHERE userId = ?",[userId]);if(!results.length)return;const wantedExp=results[0].exp+amount;if(wantedExp<0)return;await this.setExp(client,userId,wantedExp);resolve();});}
-exports.setExp=async(client,userId,amount)=>{return new Promise(async resolve=>{amount=parseInt(amount);if(isNaN(userId)||userId.length!=18||isNaN(amount)||amount<0)return;this.query("UPDATE `user` SET `exp` = ? WHERE userId = ?",[amount,userId]).then(()=>{this.checkLevel(client,userId);resolve();});});}
-exports.addLevel=async(client,userId,amount)=>{return new Promise(async resolve=>{amount=parseInt(amount);if(isNaN(userId)||userId.length!=18||isNaN(amount))return;const results=await this.query("SELECT `level` FROM `user` WHERE userId = ?",[userId]);if(!results.length)return;const wantedLevel=results[0].level+amount;if(wantedLevel<0)return;await this.setLevel(client,userId,wantedLevel);resolve();});}
-exports.setLevel=async(client,userId,amount)=>{return new Promise(async resolve=>{amount=parseInt(amount);if(isNaN(userId)||userId.length!=18||isNaN(amount)||amount<0)return;if(amount==0)return this.setExp(client,userId,amount);const neededExp=baseexp*(amount)+addexp*(amount-1);await this.setExp(client,userId,neededExp);resolve();});}
-exports.checkLevel=async(client,userId)=>{if(isNaN(userId)||userId.length!=18)return;const result=await this.query("SELECT exp, `level` FROM user WHERE userId = ?",[userId]);if(!result.length)return;const exp=result[0].exp;let level=result[0].level;while(true){let expNeeded=baseexp*(level+1)+addexp*level;let expNeededPrev=baseexp*level+addexp*(level-1);if(exp<expNeededPrev){level--;}else if(exp>=expNeeded){level++;}else{break;}}this.query("UPDATE user SET `level` = ? WHERE userId = ?",[level,userId]).then(()=>{this.checkLevelRoles(client,userId);});}
-exports.checkLevelRoles=async(client,userId)=>{if(isNaN(userId)||userId.length!=18)return;const result=await this.query("SELECT `level` FROM user WHERE userId = ?",[userId]);if(!result.length)return;const level=result[0].level;const guild=client.guilds.cache.get(botGuild);const member=guild.members.cache.get(userId);if(level>=5&&!hasRole(member,levelRoles.five)){addRole(member,levelRoles.five);}else if(level<5&&hasRole(member,levelRoles.five)){removeRole(member,levelRoles.five);}if(level>=10&&!hasRole(member,levelRoles.ten)){addRole(member,levelRoles.ten);}else if(level<10&&hasRole(member,levelRoles.ten)){removeRole(member,levelRoles.ten);}if(level>=25&&!hasRole(member,levelRoles.twentyfive)){addRole(member,levelRoles.twentyfive);}else if(level<25&&hasRole(member,levelRoles.twentyfive)){removeRole(member,levelRoles.twentyfive);}if(level>=50&&!hasRole(member,levelRoles.fifty)){addRole(member,levelRoles.fifty);}else if(level<50&&hasRole(member,levelRoles.fifty)){removeRole(member,levelRoles.fifty);}if(level>=100&&!hasRole(member,levelRoles.hundered)){addRole(member,levelRoles.hundered);}else if(level<100&&hasRole(member,levelRoles.hundered)){removeRole(member,levelRoles.hundered);}}
-exports.isBlacklisted=async(userId)=>{if(isNaN(userId)||userId.length!=18)return false;const result=await this.query("SELECT * FROM blacklisted WHERE userId = ?",[userId]);if(!result.length)return false;return true;}
-exports.makeOrderId=async()=>{let id="";for(let i=0;i<idLength;i++){id+=characters.charAt(randomInt(0,characters.length));}const result=await this.query("SELECT orderId FROM `order` WHERE orderId = ?",[id]);if(result.length){return this.makeOrderId();}else{return id;}}
-exports.deleteOrders=async(client)=>{const results=await this.query("SELECT orderId, userId, guildId FROM `order` WHERE status NOT IN('delivered','deleted')");const deletedGuilds=[];for(let result of results){const guild=client.guilds.cache.get(result.guildId);if (!guild && !deletedGuilds.includes(result.guildId)){deletedGuilds.push(result.guildId);this.query("DELETE FROM `order` WHERE guildId = ?",[result.guildId]);} else if (guild) {const member=guild.members.cache.get(result.userId);if(!member){this.query("DELETE FROM `order` WHERE userId = ?",[result.userId]);}}}}
-exports.makeApplicationId=async()=>{let id="";for(let i=0;i<idLength;i++){id+=characters.charAt(randomInt(0,characters.length));}const result=await this.query("SELECT applicationId FROM `application` WHERE applicationId = ?",[id]);if(result.length){return this.makeApplicationId();}else{return id;}}
+const mysql = require("mysql2");
+const {addRole, removeRole, hasRole, randomInt} = require('./functions');
+const {baseexp, addexp} = require('./level.json');
+const {botGuild, idLength} = require('./config.json');
+const {levelRoles} = require('./roles.json');
+const {host, user, password, database} = require('./database.json');
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+let con;
+const handleDisconnect = () => {
+    con = mysql.createConnection({host: host, user: user, password: password, database: database});
+    con.connect(err => {
+        if(err){
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+    con.on('error', err => {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST')handleDisconnect();
+        else throw err;
+    });
+}
+handleDisconnect();
+
+exports.query = (query, options = []) => new Promise((resolve, reject)=>con.execute(query, options, (error, result, fields) => {
+    if(error) reject(error);
+    else resolve({result: result, fields: fields});
+}));
+exports.addUser = userId => {
+    if(isNaN(userId) || userId.length != 18) return;
+    this.query("SELECT * FROM `user` WHERE userId = ?", [userId]).then(value => {
+        if(!value.result.length) this.query("INSERT INTO `user`(userId) VALUES(?)", [userId])
+    });
+}
+exports.checkLevelRoles = (client, userId) => {
+    if(isNaN(userId) || userId.length != 18) return;
+    this.query("SELECT `level` FROM user WHERE userId = ?", [userId]).then(value => {
+        if(value.result.length){
+            const level = value.result[0].level;
+            const member = client.guilds.cache.get(botGuild).members.cache.get(userId);
+            if(level >= 5 && !hasRole(member, levelRoles.five)){
+                addRole(member, levelRoles.five);
+            } else if(hasRole(member, levelRoles.five)){
+                removeRole(member, levelRoles.five);
+            }
+            if(level >= 10 && !hasRole(member, levelRoles.ten)){
+                addRole(member, levelRoles.ten);
+            } else if(hasRole(member, levelRoles.ten)){
+                removeRole(member, levelRoles.ten);
+            }
+            if(level >= 25 && !hasRole(member, levelRoles.twentyfive)){
+                addRole(member, levelRoles.twentyfive);
+            } else if(hasRole(member, levelRoles.twentyfive)){
+                removeRole(member, levelRoles.twentyfive);
+            }
+            if(level >= 50 && !hasRole(member, levelRoles.fifty)){
+                addRole(member, levelRoles.fifty);
+            } else if(hasRole(member, levelRoles.fifty)){
+                removeRole(member, levelRoles.fifty);
+            }
+            if(level >= 100 && !hasRole(member, levelRoles.hundered)){
+                addRole(member, levelRoles.hundered);
+            } else if(hasRole(member, levelRoles.hundered)){
+                removeRole(member, levelRoles.hundered);
+            }
+        }
+    });
+}
+exports.checkLevel = async (client, userId) => {
+    if(isNaN(userId) || userId.length != 18) return;
+    const result = await this.query("SELECT exp, `level` FROM user WHERE userId = ?", [userId]);
+    if(!result.length) return;
+    const exp = result[0].exp;
+    let level = result[0].level;
+    while(true){
+        if(exp < (baseexp * level + addexp * (level - 1))){
+            level--;
+        } else if(exp >= (baseexp * (level + 1) + addexp * level)){
+            level++;
+        } else {
+            break;
+        }
+    }
+    this.query("UPDATE user SET `level` = ? WHERE userId = ?", [level, userId]).then(()=>{
+        this.checkLevelRoles(client, userId);
+    });
+}
+exports.setExp = (client, userId, amount) => new Promise(async resolve => {
+    amount = parseInt(amount);
+    if(isNaN(userId) || userId.length != 18 || isNaN(amount) || amount < 0) return;
+    this.query("UPDATE `user` SET `exp` = ? WHERE userId = ?", [amount, userId]).then(()=>{
+        this.checkLevel(client, userId);
+        resolve();
+    });
+});
+exports.addExp = (client, userId, amount) => new Promise(async resolve => {
+    amount=parseInt(amount);
+    if(isNaN(userId) || userId.length != 18 || isNaN(amount)) return;
+    const results = await this.query("SELECT `exp` FROM `user` WHERE userId = ?", [userId]);
+    if(!results.length) return;
+    const wantedExp = results[0].exp + amount;
+    if(wantedExp < 0) return;
+    await this.setExp(client, userId, wantedExp);
+    resolve();
+});
+exports.setLevel = (client, userId, amount) => new Promise(async resolve => {
+    amount = parseInt(amount);
+    if(isNaN(userId) || userId.length != 18 || isNaN(amount) || amount < 0) return;
+    if(amount == 0) return this.setExp(client, userId, amount);
+    const neededExp = baseexp * (amount) + addexp * (amount - 1);
+    await this.setExp(client, userId, neededExp);
+    resolve();
+});
+exports.addLevel = (client, userId, amount) => new Promise(async resolve => {
+    amount = parseInt(amount);
+    if(isNaN(userId) || userId.length != 18 || isNaN(amount)) return;
+    const results = await this.query("SELECT `level` FROM `user` WHERE userId = ?", [userId]);
+    if(!results.length) return;
+    const wantedLevel = results[0].level + amount;
+    if(wantedLevel < 0) return;
+    await this.setLevel(client, userId, wantedLevel);
+    resolve();
+});
+exports.isBlacklisted = async (userId) => {
+    if(isNaN(userId) || userId.length != 18) return false;
+    const result = await this.query("SELECT * FROM blacklisted WHERE userId = ?", [userId]);
+    return result.length ? true : false;
+}
+exports.makeOrderId = async () => {
+    let id = "";
+    for(let i = 0; i < idLength; i++){
+        id += characters.charAt(randomInt(0, characters.length));   
+    }
+    const result=await this.query("SELECT orderId FROM `order` WHERE orderId = ?",[id]);
+    return result.length ? this.makeOrderId() : id;
+}
+exports.deleteOrders = async (client) => {
+    const results = await this.query("SELECT orderId, userId, guildId FROM `order` WHERE status NOT IN('delivered', 'deleted')");
+    const deletedGuilds = [];
+    for(let result of results){
+        const guild = client.guilds.cache.get(result.guildId);
+        if (!guild && !deletedGuilds.includes(result.guildId)){
+            deletedGuilds.push(result.guildId);
+            this.query("DELETE FROM `order` WHERE guildId = ?", [result.guildId]);
+        } else if (guild) {
+            const member = guild.members.cache.get(result.userId);
+            if(!member){
+                this.query("DELETE FROM `order` WHERE userId = ?", [result.userId]);
+            }
+        }
+    }
+}
+exports.makeApplicationId = async () => {
+    let id = "";
+    for(let i = 0; i < idLength; i++){
+        id += characters.charAt(randomInt(0, characters.length));
+    }
+    const result = await this.query("SELECT applicationId FROM `application` WHERE applicationId = ?", [id]);
+    return result.length ? this.makeApplicationId() : id;
+}

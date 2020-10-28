b6d3fdc65e57cb590bb8b2b61f47e5b514f5383f
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-10-28 20:14:24 +0100

    Changed main file to use new functions correctly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const {Client,Collection}=require('discord.js');
-const client=new Client();
-const {token,prefix,botGuild}=require('./config.json');
+const { Client, Collection } = require('discord.js');
+const client = new Client();
+const { token, prefix, botGuild } = require('./config.json');
 /* 
 colors I use:
 * notice: gray / lightgray
@@ -14,32 +14,32 @@ colors I use:
 * cooldown: black
 * noiceboard message: noiceboard
 */
-const {blue,green,red,black}=require('./colors.json');
-const {noice,noice2}=require('./emojis.json');
-const {text}=require('./channels.json');
-const {levelRoles}=require('./roles.json');
-const {developer,worker,teacher,staff,director}=require('./roles.json');
-const {updateMemberSize,updateGuildAmount,sendGuildLog,createEmbed,checkNoiceBoard,sendEmbed, hasRole}=require('./functions');
-const {addUser,query,addExp,isBlacklisted,deleteOrders}=require('./dbfunctions');
+const { blue, green, red, black } = require('./colors.json');
+const { noice, noice2 } = require('./emojis.json');
+const { text } = require('./channels.json');
+const { levelRoles } = require('./roles.json');
+const { developer, worker, teacher, staff, director } = require('./roles.json');
+const { updateMemberSize, updateGuildAmount, sendGuildLog, createEmbed, checkNoiceBoard, sendEmbed, hasRole, sendEmbedWithChannel, editEmbed } = require('./functions');
+const { addUser, query, addExp, isBlacklisted, deleteOrders } = require('./dbfunctions');
 const cmdFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-client.commands=new Collection();
-const cooldowns=new Collection();
-client.worker=false;
-client.teacher=false;
-client.staff=false;
-client.director=false;
-client.toggles={
-    cooldowns:true,
-    addExp:true,
-    sendEveryone:false,
-    roleChecks:true,
-    pponlyChecks:true
+client.commands = new Collection();
+const cooldowns = new Collection();
+client.worker = false;
+client.teacher = false;
+client.staff = false;
+client.director = false;
+client.toggles = {
+    cooldowns: true,
+    addExp: true,
+    sendEveryone: false,
+    roleChecks: true,
+    pponlyChecks: true
 };
-client.applications={
-    worker:true,
-    teacher:true,
-    developer:true,
-    staff:true
+client.applications = {
+    worker: true,
+    teacher: true,
+    developer: true,
+    staff: true
 };
 
 for (let file of cmdFiles) {
@@ -59,31 +59,49 @@ client.on('ready', () => {
     updateGuildAmount(client);
     updateMemberSize(client);
     const guild = client.guilds.cache.get(botGuild);
-    guild.members.cache.forEach(member=>{
-        if(!member.user.bot)addUser(member.id);
-    });
+    guild.members.cache.forEach(member => { if (!member.user.bot) addUser(member.id) });
     query("UPDATE `order` SET status = 'cooked' WHERE status = 'cooking'");
     console.log("Pixel Pizza is ready");
 });
 
 client.on('guildCreate', guild => {
     updateGuildAmount(client);
-    sendGuildLog(guild.name, guild.iconURL(), createEmbed(green, "Added", null, null, `${client.user.username} has been added to the guild ${guild.name}`, null, [], null, true, {text: guild.id}));
+    sendGuildLog(guild.name, guild.iconURL(), createEmbed({
+        color: green,
+        title: "Added",
+        description: `${client.user.username} has been added to the guild ${guild.name}`,
+        timestamp: true,
+        footer: {
+            text: guild.id
+        }
+    }));
     let channel = guild.systemChannel;
-    if (!channel){
+    if (!channel) {
         channel = guild.channels.cache.find(channel => channel.type === "text");
     }
-    channel.send(createEmbed(green, "Thank you!", null, null, `Thank you for adding me!\nMy prefix is ${prefix}\nUse ${prefix}help for all commands!`));
+    sendEmbedWithChannel(createEmbed({
+        color: green,
+        title: "Thank you!",
+        description: `Thank you for adding me!\nMy prefix is ${prefix}\nUse ${prefix}help for all commands!`
+    }), client, channel);
 });
 
 client.on('guildDelete', guild => {
     updateGuildAmount(client);
-    sendGuildLog(guild.name, guild.iconURL(), createEmbed(red, "Removed", null, null, `${client.user.username} has been removed from the guild ${guild.name}`, null, [], null, true, {text: guild.id}));
+    sendGuildLog(guild.name, guild.iconURL(), createEmbed({
+        color: red,
+        title: "Removed",
+        description: `${client.user.username} has been removed from the guild ${guild.name}`,
+        timestamp: true,
+        footer: {
+            text: guild.id
+        }
+    }));
 });
 
 client.on('guildMemberAdd', member => {
     if (member.guild.id !== botGuild) return;
-    if (!member.user.bot)addUser(member.id);
+    if (!member.user.bot) addUser(member.id);
     updateMemberSize(client);
 });
 
@@ -103,20 +121,20 @@ client.on('messageReactionRemove', messageReaction => {
 });
 
 client.on('message', async message => {
-    deleteOrders(client).then(async()=>{
+    deleteOrders(client).then(async () => {
         client.guild = client.guilds.cache.get(botGuild);
         client.member = client.guild.members.cache.get(message.author.id);
         const guild = client.guild;
         const member = client.member;
-        if (message.channel.id === text.logs && !message.webhookID){
+        if (message.channel.id === text.logs && !message.webhookID) {
             return message.delete();
         }
-        if (message.channel.id === text.updates && message.member){
-            if (!message.member.roles.cache.get(developer)){
+        if (message.channel.id === text.updates && message.member) {
+            if (!message.member.roles.cache.get(developer)) {
                 message.delete();
             }
         }
-        if (message.guild == guild && client.toggles.addExp && message.author != client.user)await addExp(client,message.author.id,1);
+        if (message.guild == guild && client.toggles.addExp && message.author != client.user) await addExp(client, message.author.id, 1);
         if (message.content.toLowerCase().includes('noice')) {
             message.react(noice).then(console.log).catch(console.error);
         }
@@ -125,7 +143,7 @@ client.on('message', async message => {
         if (message.author.bot && message.content != "pptoggle sendEveryone") return;
         let clientMember;
         client.canSendEmbeds = true;
-        if (message.guild){
+        if (message.guild) {
             clientMember = message.guild.members.cache.get(client.user.id);
             if (!clientMember.hasPermission("EMBED_LINKS")) client.canSendEmbeds = false;
         }
@@ -134,59 +152,78 @@ client.on('message', async message => {
         console.log(commandName);
         const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
         if (!command) return;
-        let embedMsg = createEmbed(blue,null,null,{name:message.author.username,icon:message.author.displayAvatarURL()}, null, message.author.displayAvatarURL(), [], null, true, {text:client.user.username,icon:client.user.displayAvatarURL()});
+        let embedMsg = createEmbed({
+            color: red,
+            author: {
+                name: message.author.username,
+                icon: message.author.displayAvatarURL()
+            },
+            thumbnail: message.author.displayAvatarURL(),
+            timestamp: true,
+            footer: {
+                text: client.user.username,
+                icon: client.user.displayAvatarURL()
+            }
+        });
         if (await isBlacklisted(message.author.id)) return;
         if (message.channel.type == "dm") {
-            embedMsg.setColor(red).setDescription("Our commands are unavailable in DMs");
-            return sendEmbed(embedMsg,message);
+            return sendEmbed(editEmbed(embedMsg, {
+                description: "Our commands are unavailable in DMs"
+            }), message);
         }
-        if (command.removeExp && message.guild == client.guild && client.toggles.addExp && !message.author.bot)await addExp(client,message.author.id,-1);
-        if (command.ppOnly && message.guild != guild){
-            embedMsg.setColor(red).setDescription(`This command can only be used in ${guild.name}`);
-            return sendEmbed(embedMsg,message);
+        if (command.removeExp && message.guild == client.guild && client.toggles.addExp && !message.author.bot) await addExp(client, message.author.id, -1);
+        if (command.ppOnly && message.guild != guild) {
+            return sendEmbed(editEmbed(embedMsg, {
+                description: `This command can only be used in ${guild.name}`
+            }), message);
         }
-        if (member){
+        if (member) {
             worker.forEach(role => {
-                if (member.roles.cache.get(role)){
+                if (member.roles.cache.get(role)) {
                     client.worker = true;
                 }
             });
             teacher.forEach(role => {
-                if (member.roles.cache.get(role)){
+                if (member.roles.cache.get(role)) {
                     client.teacher = true;
                 }
             });
             staff.forEach(role => {
-                if (member.roles.cache.get(role)){
+                if (member.roles.cache.get(role)) {
                     client.staff = true;
                 }
             });
             director.forEach(role => {
-                if (member.roles.cache.get(role)){
+                if (member.roles.cache.get(role)) {
                     client.director = true;
                     client.staff = true;
                 }
             });
         }
-        if (command.userType == "worker" && !client.worker){
-            embedMsg.setColor(red).setDescription("You need to be Pixel Pizza worker to use this command!");
-            return sendEmbed(embedMsg,message);
+        if (command.userType == "worker" && !client.worker) {
+            return sendEmbed(editEmbed(embedMsg, {
+                description: "You need to be Pixel Pizza worker to use this command!"
+            }), message);
         }
-        if (command.userType == "teacher" && !client.teacher){
-            embedMsg.setColor(red).setDescription("You need to be Pixel Pizza teacher to use this command!");
-            return sendEmbed(embedMsg,message);
+        if (command.userType == "teacher" && !client.teacher) {
+            return sendEmbed(editEmbed(embedMsg, {
+                description: "You need to be Pixel Pizza teacher to use this command!"
+            }), message);
         }
-        if (command.userType == "staff" && !client.staff){
-            embedMsg.setColor(red).setDescription("You need to be Pixel Pizza staff to use this command!");
-            return sendEmbed(embedMsg,message);
+        if (command.userType == "staff" && !client.staff) {
+            return sendEmbed(editEmbed(embedMsg, {
+                description: "You need to be Pixel Pizza staff to use this command!"
+            }), message);
         }
-        if (command.userType == "director" && !client.director){
-            embedMsg.setColor(red).setDescription("You need to be Pixel Pizza director to use this command!");
-            return sendEmbed(embedMsg,message);
+        if (command.userType == "director" && !client.director) {
+            return sendEmbed(editEmbed(embedMsg, {
+                description: "You need to be Pixel Pizza director to use this command!"
+            }), message);
         }
-        if(command.needVip && !hasRole(member,levelRoles.hundered)){
-            embedMsg.setColor(red).setDescription("You need to have the vip role in pixel pizza to use this command!");
-            return sendEmbed(embedMsg,message);
+        if (command.needVip && !hasRole(member, levelRoles.hundered)) {
+            return sendEmbed(editEmbed(embedMsg, {
+                description: "You need to have the vip role in pixel pizza to use this command!"
+            }), message);
         }
         let reply;
         if (command.args && !args.length) {
@@ -194,51 +231,62 @@ client.on('message', async message => {
             if (command.usage) {
                 reply += `\nThe proper usage is: '${prefix}${command.name} ${command.usage}'`;
             }
-            embedMsg.setColor(red).setTitle('**No arguments**').setDescription(reply);
-            return sendEmbed(embedMsg,message);
+            return sendEmbed(editEmbed(embedMsg, {
+                title: '**No arguments**',
+                description: reply
+            }), message);
         }
-        if (command.args == false && args.length){
-            embedMsg.setColor(red).setTitle('**No arguments needed**').setDescription(`This command doesn't require any arguments, ${message.author}`);
-            return sendEmbed(embedMsg,message);
+        if (command.args == false && args.length) {
+            return sendEmbed(editEmbed(embedMsg, {
+                title: '**No arguments needed**',
+                description: `This command doesn't require any arguments, ${message.author}`
+            }), message);
         }
-        if (command.minArgs && args.length < command.minArgs){
+        if (command.minArgs && args.length < command.minArgs) {
             reply = `${prefix}${command.name} takes a minimum of ${command.minArgs} argument(s)`;
-            if (command.usage){
+            if (command.usage) {
                 reply += `\nThe proper usage is ${prefix}${command.name} ${command.usage}`;
             }
-            embedMsg.setColor(red).setDescription(reply);
-            return sendEmbed(embedMsg,message);
+            return sendEmbed(editEmbed(embedMsg, {
+                description: reply
+            }), message);
         }
-        if (command.maxArgs && args.length > command.maxArgs){
+        if (command.maxArgs && args.length > command.maxArgs) {
             reply = `${prefix}${command.name} takes a maximum of ${command.maxArgs} argument(s)`;
-            if (command.usage){
+            if (command.usage) {
                 reply += `The proper usage is ${prefix}${command.name} ${command.usage}`;
-            } 
-            embedMsg.setColor(red).setDescription(reply);
-            return sendEmbed(embedMsg,message);
+            }
+            return sendEmbed(editEmbed(embedMsg, {
+                description: reply
+            }), message);
         }
-        if (command.neededPerms && command.neededPerms.length){
-            for(let index in command.neededPerms){
+        if (command.neededPerms && command.neededPerms.length) {
+            for (let index in command.neededPerms) {
                 let neededPerm = command.neededPerms[index];
-                if (!clientMember.hasPermission(neededPerm)){
-                    let embedMsgError = createEmbed(red, "Missing permission", null, null, `I'm missing the \`${neededPerm}\` permission\nIf you want to know why this permission is needed please DM Jaron#3021`);
-                    return sendEmbed(embedMsgError,message);
+                if (!clientMember.hasPermission(neededPerm)) {
+                    return sendEmbed(editEmbed(embedMsg, {
+                        title: "Missing permission",
+                        description: `I'm missing the \`${neededPerm}\` permission\nIf you want to know why this permission is needed please DM Jaron#3021`
+                    }), message);
                 }
             }
         }
-        if(client.toggles.cooldowns){
+        if (client.toggles.cooldowns) {
             if (!cooldowns.has(command.name)) {
                 cooldowns.set(command.name, new Collection());
             }
             const now = Date.now();
             const timestamps = cooldowns.get(command.name);
             let cooldownAmount = (command.cooldown || 0) * 1000;
-            if (timestamps.has(message.author.id)){
+            if (timestamps.has(message.author.id)) {
                 const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
                 if (now < expirationTime) {
                     const timeLeft = (expirationTime - now) / 1000;
-                    embedMsg.setColor(black).setTitle('**Cooldown**').setDescription(`please wait ${timeLeft} more second(s) before reusing ${command.name}`);
-                    return sendEmbed(embedMsg,message);
+                    return sendEmbed(editEmbed(embedMsg, {
+                        color: black,
+                        title: '**Cooldown**',
+                        description: `please wait ${timeLeft} more second(s) before reusing ${command.name}`
+                    }), message);
                 }
             }
             timestamps.set(message.author.id, now);
@@ -249,8 +297,10 @@ client.on('message', async message => {
             console.log(`${command.name} executed!`);
         } catch (error) {
             console.error(error);
-            embedMsg.setColor(red).setTitle('**Error**').setDescription('there was an error trying to execute that command!');
-            return sendEmbed(embedMsg,message);
+            return sendEmbed(editEmbed(embedMsg, {
+                title: '**Error**',
+                description: 'there was an error trying to execute that command!'
+            }), message);
         }
     }).catch(error => {
         console.error(error);

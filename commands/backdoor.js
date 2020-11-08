e534adfe069cc136524a24d808095fb2c8193877
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-11-08 14:35:03 +0100

    Finished command to get invite of a server
    This is part of isssue #22

---
+++ commands/backdoor.js
@@ -0,0 +1,26 @@
+const { createEmbed, getGuild, sendEmbed, capitalize } = require("../functions");
+const { red } = require('../colors.json');
+
+module.exports = {
+    name: "backdoor",
+    description: "get invite link of a guild (this is used for if the invite is lost or has been expired)",
+    args: true,
+    minArgs: 1,
+    usage: "<guild>",
+    cooldown: 0,
+    userType: "staff",
+    neededPerms: ["CREATE_INSTANT_INVITE"],
+    pponly: true,
+    removeExp: false,
+    async execute(message, args, client) {
+        const guild = getGuild(args, client);
+        if(!guild){
+            return sendEmbed(createEmbed({
+                color: red,
+                title: `**${capitalize(this.name)}**`,
+                description: 'Could not find guild, please be more specific'
+            }));
+        }
+        message.channel.send(await guild.channels.cache.find(channel => channel.type == "text").createInvite({ maxAge: 0, maxUses: 1 }).url);
+    }
+}

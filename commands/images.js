1316cc23292ba7bcb5e19506fc22db66a93b6509
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-11-13 22:21:55 +0100

    Improved images command
    This is part of issue #156

--- commands/images.js
+++ commands/images.js
@@ -16,59 +16,61 @@ module.exports = {
     removeExp: false,
     needVip: false,
     async execute(message, args, client) {
-        gis(args.join(' '), (error, results) => {
-            if(error) throw error;
-            const pages = [];
-            results.forEach((result) => {
-                if(client.canSendEmbeds){
-                    pages.push(createEmbed({
-                        color: blue,
-                        title: `**Image**`,
-                        description: args.join(" "),
-                        image: result.url,
-                        fields: [
-                            {
-                                name: `URL`,
-                                value: result.url
-                            }, 
-                            {
-                                name: `Width`,
-                                value: `${result.width} pixels`
-                            }, 
-                            {
-                                name: `Height`,
-                                value: `${result.height} pixels`
-                            }
-                        ]
-                    }));
-                } else {
-                    pages.push(result.url);
-                }
-            });
-            message.channel.send(pages[0]).then(msg => msg.react('⏪').then(() => msg.react('⏩').then(() => {
-                let page = 0;
-                msg.createReactionCollector((reaction, user) => message.author.id === user.id && ['⏪', '⏩'].includes(reaction.emoji.name)).on('collect', (reaction) => {
-                    switch(reaction.emoji.name){
-                        case '⏪':
-                            if(page == 0){
-                                page = pages.length-1;
-                            } else {
-                                page--;
-                            }
-                            break;
-                        case '⏩':
-                            if(page == pages.length-1){
-                                page = 0;
-                            } else {
-                                page++;
-                            }
-                            break;
-                        default:
-                            return;
+        message.channel.send('Searching for images').then(msg => {
+            gis(args.join(' '), (error, results) => {
+                if(error) throw error;
+                const pages = [];
+                results.forEach((result) => {
+                    if(client.canSendEmbeds){
+                        pages.push(createEmbed({
+                            color: blue,
+                            title: `**Image**`,
+                            description: args.join(" "),
+                            image: result.url,
+                            fields: [
+                                {
+                                    name: `URL`,
+                                    value: result.url
+                                }, 
+                                {
+                                    name: `Width`,
+                                    value: `${result.width} pixels`
+                                }, 
+                                {
+                                    name: `Height`,
+                                    value: `${result.height} pixels`
+                                }
+                            ]
+                        }));
+                    } else {
+                        pages.push(result.url);
                     }
-                    msg.edit(pages[page]).then(() => msg.reactions.removeAll().then(() => msg.react('⏪').then(() => msg.react('⏩'))));
                 });
-            })));
+                msg.edit().then(() => msg.react('⏪').then(() => msg.react('⏩').then(() => {
+                    let page = 0;
+                    msg.createReactionCollector((reaction, user) => message.author.id === user.id && ['⏪', '⏩'].includes(reaction.emoji.name)).on('collect', (reaction) => {
+                        switch(reaction.emoji.name){
+                            case '⏪':
+                                if(page == 0){
+                                    page = pages.length-1;
+                                } else {
+                                    page--;
+                                }
+                                break;
+                            case '⏩':
+                                if(page == pages.length-1){
+                                    page = 0;
+                                } else {
+                                    page++;
+                                }
+                                break;
+                            default:
+                                return;
+                        }
+                        msg.edit(pages[page]).then(() => msg.reactions.removeAll().then(() => msg.react('⏪').then(() => msg.react('⏩'))));
+                    });
+                })));
+            });
         });
     }
 }

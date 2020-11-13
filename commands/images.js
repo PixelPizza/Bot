e9e242e5f4d3d94c9f9223d79f74906960eaaa99
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-11-13 21:40:30 +0100

    Made first version of images command

---
+++ commands/images.js
@@ -0,0 +1,21 @@
+const gis = require('g-i-s');
+
+module.exports = {
+    name: "images",
+    description: "search for images",
+    aliases: [],
+    args: true,
+    minArgs: 1,
+    usage: "<search>",
+    cooldown: 0,
+    userType: "worker",
+    neededPerms: [],
+    pponly: false,
+    removeExp: false,
+    needVip: false,
+    async execute(message, args, client) {
+        gis(args.join(' '), (error, results) => {
+            message.channel.send(results.map(result => result.url).join("\n"));
+        });
+    }
+}

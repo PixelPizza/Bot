bdd8babb163a599a07e9d419dcbe5637cb980226
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-11-21 11:12:33 +0100

    Added ping command
    This solves issue #166

---
+++ commands/ping.js
@@ -0,0 +1,29 @@
+const { sendEmbed, createEmbed, capitalize } = require("../functions");
+const { blue } = require('../colors.json');
+
+module.exports = {
+    name: "ping",
+    description: "ping the bot",
+    args: false,
+    cooldown: 60,
+    userType: "all",
+    neededPerms: [],
+    pponly: false,
+    removeExp: false,
+    needVip: false,
+    async execute(message, args, client) {
+        sendEmbed(createEmbed({
+            color: blue,
+            fields: [
+                {
+                    name: "Message Latency",
+                    value: `${Date.now() - message.createdTimestamp}ms`
+                },
+                {
+                    name: "Bot Latency",
+                    value: `${Math.round(client.ws.ping)}ms`
+                }
+            ]
+        }), message);
+    }
+}

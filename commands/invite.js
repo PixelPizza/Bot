26f2e76e78dcedeb7bdc90532e2d7e4a2c634992
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-11-21 10:12:01 +0100

    Added invite command
    This is part of issue #167

---
+++ commands/invite.js
@@ -0,0 +1,24 @@
+const {Permissions} = require('discord.js');
+const { sendEmbed, createEmbed, capitalize } = require('../functions');
+const { blue } = require('../colors.json');
+const FLAGS = Permissions.FLAGS;
+
+module.exports = {
+    name: "invite",
+    description: "invite the bot",
+    args: false,
+    cooldown: 0,
+    userType: "all",
+    neededPerms: [],
+    pponly: false,
+    removeExp: false,
+    needVip: false,
+    async execute(message, args, client) {
+        const permInt = FLAGS.CREATE_INSTANT_INVITE | FLAGS.SEND_MESSAGES | FLAGS.EMBED_LINKS;
+        sendEmbed(createEmbed({
+            color: blue,
+            title: `**${capitalize(this.name)}**`,
+            description: `Here is the invite link for ${client.user.username}!\nhttps://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=${permInt}&scope=bot`
+        }), message);
+    }
+}

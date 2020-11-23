70318dc2199adff1353980b0ecafe22f74b2e240
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-11-23 23:17:27 +0100

    Resolve #172

--- commands/order.js
+++ commands/order.js
@@ -11,7 +11,7 @@ module.exports = {
     description: "order a pizza", 
     args: true, 
     minArgs: 1, 
-    usage: "<order>", 
+    usage: "<order> [{chef: chef, deliverer: deliverer} | {chef: chef} | {deliverer: deliverer}]", 
     cooldown: 0, 
     userType: "all", 
     neededPerms: ["CREATE_INSTANT_INVITE"], 
@@ -23,6 +23,34 @@ module.exports = {
             title: `**${capitalize(this.name)}**`,
             description: "Your pizza has been ordered and will be cooked as soon as possible"
         });
+        let chef = null;
+        let deliverer = null;
+        const selection = /^.*?({chef:(?<chef>.+?), *deliverer:(?<deliverer>.+?)}|{chef:(?<chefsingle>.+?)}|{deliverer:(?<deliverersingle>.+?)})$/.exec(message);
+        if(selection){
+            args = args.reverse();
+            args.splice(0, selection[1].split(/ +/).length);
+            const select = {
+                chef: (selection.groups.chef || selection.groups.chefsingle)?.trim(),
+                deliverer: (selection.groups.deliverer || selection.groups.deliverersingle)?.trim()
+            };
+            args = args.reverse();
+            chef = client.guildMembers.find(member => member.user.id == select.chef || member.user.username.toLowerCase().includes(select.chef?.toLowerCase()) || member.displayName.toLowerCase().includes(select.chef?.toLowerCase()));
+            deliverer = client.guildMembers.find(member => member.user.id == select.deliverer || member.user.username.toLowerCase().includes(select.deliverer?.toLowerCase()) || member.displayName.toLowerCase().includes(select.deliverer?.toLowerCase()));
+            const chefResult = await query("SELECT * FROM worker WHERE workerId = ?", [chef?.id || null]);
+            const deliverResult = await query("SELECT * FROM worker WHERE workerId = ?", [deliverer?.id || null]);
+            if(select.chef && !chefResult.length){
+                return sendEmbed(editEmbed(embedMsg, {
+                    title: "Chef not found",
+                    description: "Could not find chef"
+                }), message);
+            }
+            if(select.deliverer && !deliverResult.length){
+                return sendEmbed(editEmbed(embedMsg, {
+                    title: "Deliverer not found",
+                    description: "Could not find deliverer"
+                }), message);
+            }
+        }
         let result = await query("SELECT COUNT(*) as counted FROM `order` WHERE status NOT IN('delivered', 'deleted')"); 
         if (result[0].counted >= maxPizzas && !isVip(client.member)) { 
             return sendEmbed(editEmbed(embedMsg, {
@@ -55,14 +83,23 @@ module.exports = {
             order = `Random pizza with these things: ${chosenIngredients.join(", ")}`;
         }
         const id = await makeOrderId(); 
-        await query("INSERT INTO `order`(orderId,userId,guildId,channelId,status,`order`) VALUES(?,?,?,?,'not claimed',?)", [id, message.author.id, message.guild.id, message.channel.id, order]); 
+        const selections = "(orderId,userId,guildId,channelId,status,`order`" + (chef ? ",cookId" : "") + (deliverer ? ",delivererId" : "") + ")";
+        const values = "(?,?,?,?,'not claimed',?" + (chef ? ",?" : "") + (deliverer ? ",?" : "") + ")";
+        const options = [id, message.author.id, message.guild.id, message.channel.id, order];
+        if(deliverer){
+            options.push(deliverer.id);
+        }
+        if(chef){
+            options.push(chef.id);
+        }
+        await query("INSERT INTO `order`" + selections + " VALUES" + values, options); 
         const embedMsgOrder = createEmbed({
             color: blue,
             title: `**${capitalize(this.name)}**`,
             description: `a new order has come in!`,
             timestamp: true,
             footer: {
-                text: `id: ${id}`
+                text: `id: ${id}${chef ? ` | chef: ${chef.user.username}` : ""}${deliverer ? ` | deliverer: ${deliverer.user.username}` : ""}`
             }
         });
         const channel = client.channels.cache.get(text.kitchen); 

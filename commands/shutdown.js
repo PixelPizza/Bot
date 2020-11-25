ef97c0c9f4e252969bdde4d89d5458f6424ab1d1
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-11-25 15:21:58 +0100

    Changed shutdown command

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -2,12 +2,12 @@ module.exports = {
     name: "shutdown",
     description: "shut the bot down",
     args: false,
-    cooldown: 0,
+    cooldown: 100000000000,
     userType: "all",
     neededPerms: [],
     pponly: false,
     removeExp: false,
     async execute(message, args, client) {
-        message.channel.send("lol no");
+        message.channel.send("I don't think so... don't try again");
     }
 }

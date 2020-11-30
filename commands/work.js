a8dcad0333fd7a7fd68196ed69cb1a58fc841ca9
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-11-30 21:22:25 +0100

    Fixed work issues

--- commands/work.js
+++ commands/work.js
@@ -1,5 +1,6 @@
 const { randomInt } = require("crypto");
 const { config, sendEmbed, createEmbed, colors, capitalize } = require("pixel-pizza");
+const { botGuild } = require("pixel-pizza/src/data/config");
 const { query } = require('../dbfunctions');
 
 module.exports = {
@@ -14,9 +15,10 @@ module.exports = {
     pponly: false,
     removeExp: false,
     async execute(message, args, client) {
+        const guild = client.guilds.cache.get(botGuild);
         const earning = randomInt(config.minWorkEarning, config.maxWorkEarning);
         const causeIndex = randomInt(config.workCauses.length);
-        const cause = config.workCauses[causeIndex].replace("{earning}", `${config.currency}${earning}`);
+        const cause = config.workCauses[causeIndex].replace(/{earning}/g, `${config.currency}${earning}`).replace(/{botguild}/g, guild.name);
         console.log(await query("UPDATE `user` SET balance = balance + ? WHERE userId = ?", [earning, message.author.id]));
         sendEmbed(createEmbed({
             color: colors.blue.hex,

243d73f17c36e80f1ef30fae5518258be0550763
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-12-01 22:40:54 +0100

    Added bot deliveries

--- commands/deliverpersonal.js
+++ commands/deliverpersonal.js
@@ -1,5 +1,4 @@
 const { randomInt } = require("crypto");
-const { Permissions } = require("discord.js");
 const PixelPizza = require("pixel-pizza");
 const { createEmbed, hasRole, sendEmbed, editEmbed, isVip, setCooldown } = PixelPizza;
 const { blue, red } = PixelPizza.colors; 
@@ -7,20 +6,19 @@ const { deliverer, ceo } = PixelPizza.roles;
 const { query, checkProDeliverer } = require("../dbfunctions"); 
 const { orderCooldown, invite } = PixelPizza.config;
 
-module.exports = {
-    name: "deliver",
-    description: "let the bot deliver an order",
-    aliases: ["del"],
-    args: true,
-    minArgs: 1,
-    maxArgs: 1,
-    usage: "<order id>",
-    cooldown: 0,
-    userType: "worker",
-    neededPerms: [],
-    pponly: false,
-    removeExp: false,
-    async execute(message, args, client) {
+module.exports = { 
+    name: "deliverpersonal", 
+    description: "deliver an order personally (can be done once a day)", 
+    aliases: ["delpersonal"], 
+    args: true, 
+    minArgs: 1, 
+    maxArgs: 1, 
+    usage: "<order id>", 
+    cooldown: 24 * 60 * 60, 
+    userType: "worker", 
+    neededPerms: [], 
+    pponly: false, 
+    async execute(message, args, client) { 
         let embedMsg = createEmbed({ 
             color: red.hex, 
             title: "deliver" 
@@ -36,7 +34,7 @@ module.exports = {
             return sendEmbed(editEmbed(embedMsg, {
                 description: `You have not set a delivery message yet. please set one with ppdelset`
             }), message);
-        }
+        } 
         let result = results[0]; 
         let deliveryMessage = result.deliveryMessage; 
         results = await query("SELECT * FROM `order` WHERE orderId = ? AND status = 'cooked'", [args[0]]); 
@@ -44,7 +42,7 @@ module.exports = {
             return sendEmbed(editEmbed(embedMsg, {
                 description: `Order ${args[0]} has not been found with the cooked status`
             }), message);
-        }
+        } 
         result = results[0]; 
         const orderer = client.users.cache.get(result.userId); 
         if(!hasRole(client.member, ceo)){
@@ -59,11 +57,12 @@ module.exports = {
         if (result.cookId) cook = client.guildMembers.get(result.cookId) ? client.users.cache.get(result.cookId).username : "Deleted Cook"; 
         let image = result.imageUrl; 
         let guild = client.guilds.cache.get(result.guildId); 
-        let channel = client.channels.cache.get(result.channelId) ?? guild.channels.cache.find(channel => channel.type == "text" && channel.permissionsFor(channel.guild.me).has(Permissions.FLAGS.SEND_MESSAGES)); 
+        let channel = client.channels.cache.get(result.channelId) ?? guild.systemChannel; 
         const orderDate = new Date(result.orderedAt);
         const cookDate = new Date(result.cookedAt);
         const deliverDate = new Date(Date.now());
-        deliveryMessage = deliveryMessage
+        channel.createInvite({ maxAge: 0, reason: "Delivering an order" }).then(guildInvite => { 
+            deliveryMessage = deliveryMessage
             .replace(/{chef}/g, cook)
             .replace(/{customer}/g, `\`<@${orderer.id}>\``)
             .replace(/{image}/g, image)
@@ -75,13 +74,19 @@ module.exports = {
             .replace(/{orderdate}/g, `${orderDate.getDate()}-${orderDate.getMonth()}-${orderDate.getFullYear()} (dd-mm-YYYY)`)
             .replace(/{cookdate}/g, `${cookDate.getDate()}-${cookDate.getMonth()}-${cookDate.getFullYear()} (dd-mm-YYYY)`)
             .replace(/{deliverydate}/g, `${deliverDate.getDate()}-${deliverDate.getMonth()}-${deliverDate.getFullYear()} (dd-mm-YYYY)`);
-        try {
-            channel.send(deliveryMessage);
-        } catch (err) {
-            orderer.send("Could not send order to the server it was ordered in, here is your order\n"+deliveryMessage);
-            PixelPizza.error("Could not send order", err);
-        }
-        if(!member || !isVip(member)) setCooldown(client, "order", orderer.id, orderCooldown);
-        checkProDeliverer(client.guildMembers.get(message.author.id));
-    }
+            message.author.send(deliveryMessage).then(() => { 
+                message.author.send(`Don't send this link to the orderer!\n${guildInvite.url}`).then(() => { 
+                    query("UPDATE `order` SET status = 'delivered', delivererId = ?, deliveredAt = CURRENT_TIMESTAMP WHERE orderId = ?", [message.author.id, args[0]]); 
+                    query("UPDATE worker SET deliveries = deliveries + 1 WHERE workerId = ?", [message.author.id]);
+                    orderer.send(editEmbed(embedMsg, {
+                        color: blue.hex,
+                        title: `Confirmation`,
+                        description: `Your order is now being delivered by ${message.author}`
+                    })); 
+                    if(!member || !isVip(member)) setCooldown(client, "order", orderer.id, orderCooldown);
+                    checkProDeliverer(client.guildMembers.get(message.author.id));
+                }); 
+            }); 
+        }); 
+    } 
 }

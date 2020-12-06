9e3efa5deed1b8b6c300e2bdfd86b730944a93d7
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-12-06 11:14:20 +0100

    Fixed sendEmbed issues

--- pixel-pizza/src/functions/sendEmbed.js
+++ pixel-pizza/src/functions/sendEmbed.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const { Message, TextChannel, MessageEmbed } = require("discord.js");
+const { Message, TextChannel } = require("discord.js");
 
 /**
  * send an embed to a channel
- * @returns {void}
+ * @returns {Promise<Message>}
  */
 const sendEmbed = function(){
     const embed = arguments[0];
     const client = arguments[1];
     if(arguments[2] instanceof Message){
-        arguments[2].channel.send(client.canSendEmbeds ? embed : embed.description);
+        return arguments[2].channel.send(client.canSendEmbeds ? embed : embed.description);
     } else if (arguments[2] instanceof TextChannel){
-        arguments[2].send(client.canSendEmbeds ? embed : embed.description);
+        return arguments[2].send(client.canSendEmbeds ? embed : embed.description);
     }
 }
 

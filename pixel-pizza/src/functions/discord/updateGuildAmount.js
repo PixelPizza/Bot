da53851b5f880cd2351e82e6e9e6d73d7855899a
jaronetje <60853956+jaronetje@users.noreply.github.com>
2020-12-15 12:39:49 +0100

    Changed bot status to always streaming

--- pixel-pizza/src/functions/discord/updateGuildAmount.js
+++ pixel-pizza/src/functions/discord/updateGuildAmount.js
@@ -8,8 +8,6 @@ const {prefix} = require("../../data/config");
  * @returns {void}
  */
 const updateGuildAmount = (client) => {
-    const activities = ["PLAYING", "STREAMING", "LISTENING", "WATCHING"];
-    const activity = activities[Math.floor(Math.random() * activities.length)];
     let serverAmount = client.guilds.cache.array().length;
     let suffixUsed = "";
     for (let suffix in ["k", "m", "b"]) {
@@ -18,9 +16,8 @@ const updateGuildAmount = (client) => {
             suffixUsed = suffix;
         } else break;
     }
-    serverAmount = serverAmount + suffixUsed;
-    serverAmount = activity == "PLAYING" || activity == "STREAMING" ? `with ${serverAmount}` : serverAmount;
-    client.user.setActivity(`${serverAmount} guilds | ${prefix}help`, { type: activity, url: "http://twitch.tv/" });
+    serverAmount = "with " + serverAmount + suffixUsed;
+    client.user.setActivity(`${serverAmount} guilds | ${prefix}help`, { type: "STREAMING", url: "http://twitch.tv/PixelPizzaServer" });
 }
 
 module.exports = updateGuildAmount;

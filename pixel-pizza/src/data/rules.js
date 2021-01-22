37a67d1b4fc2ae6b0d58a90d799748f0be046321
jaronetje <60853956+jaronetje@users.noreply.github.com>
2021-01-22 12:51:06 +0100

    Updated the rules

--- pixel-pizza/src/data/rules.js
+++ pixel-pizza/src/data/rules.js
@@ -15,31 +15,31 @@ const anarchyGenerator = infinite();
  */
 let rules = [
     "No NSFW",
-    "No orders that are offensive or are related/imply any form of discrimination",
-    "No orders related to child exploitation (this includes Pedophilia, Child abuse, or any other form of exploitation to minors)",
-    "No controversial or political themed orders (however orders with a politician's face on them are allowed)",
-    "No orders which relate to extreme ideologies or violent groups such as Hitler/Nazis and communism",
+    "No items that are offensive or are related/imply any form of discrimination",
+    "No items related to child exploitation (this includes Pedophilia, Child abuse, or any other form of exploitation to minors)",
+    "No controversial or political themed items (however orders with a politician's face on them are allowed)",
+    "No items which relate to extreme ideologies or violent groups such as Hitler/Nazis and communism",
     "No illegal drugs (excluding weed)",
-    "No orders that are related to death, depression, disorders, or mortal illnesses",
-    "No orders which include gore/blood",
+    "No items that are related to death, depression, disorders, or mortal illnesses",
+    "No items which include gore/blood",
     "No poisons or other kinds of lethal substances (excluding bleach)",
     "No human flesh or human/animal body parts (however pictures of whole humans and animals are allowed)",
-    "No spoiler orders",
+    "No spoiler items",
     "No orders that contain more than 5 items/requests (Base counts as an item)",
-    "Must include an order (transparent & invisible orders are not allowed either)",
+    "Must include a food item (transparent & invisible orders are not allowed either)",
     "Do not attempt to bypass the word blacklist",
     "Use COMMON SENSE",
     "pixel Pizza must be in the guild in order to order",
-    "no asking for your pfp on an order"
+    "no asking for your pfp on an item"
 ];
 
 /**
  * The anarchy rules of the bot
  */
 let anarchyRules = [
-    "No orders related to child exploitation (this includes Pedophilia, Child abuse, or any other form of exploitation to minors)",
-    "No orders that are related to death, depression, disorders, or mortal illnesses",
-    "No orders which includes gore"
+    "No items related to child exploitation (this includes Pedophilia, Child abuse, or any other form of exploitation to minors)",
+    "No items that are related to death, depression, disorders, or mortal illnesses",
+    "No items which includes gore"
 ];
 
 for(let index in rules){

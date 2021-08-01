f99e939d6aaa00c07a193a2b590abb2ad67733cf
Jaron <jaron_zijlstra@outlook.com>
2021-08-01 10:51:19 +0200

    Disabled member stats

--- pixel-pizza/src/functions/discord/updateMemberSize.js
+++ pixel-pizza/src/functions/discord/updateMemberSize.js
@@ -8,10 +8,10 @@ const {voice} = require("../../data/channels");
  * @returns {void}
  */
 const updateMemberSize = (client) => {
-    const [bots, members] = client.guildMembers.partition(member => member.user.bot);
-    client.channels.cache.get(voice.allMembers).setName(`All members: ${client.guildMembers.size}`);
-    client.channels.cache.get(voice.members).setName(`Members: ${members.size}`);
-    client.channels.cache.get(voice.bots).setName(`Bots: ${bots.size}`);
+    //const [bots, members] = client.guildMembers.partition(member => member.user.bot);
+    //client.channels.cache.get(voice.allMembers).setName(`All members: ${client.guildMembers.size}`);
+    //client.channels.cache.get(voice.members).setName(`Members: ${members.size}`);
+    //client.channels.cache.get(voice.bots).setName(`Bots: ${bots.size}`);
 }
 
 module.exports = updateMemberSize;

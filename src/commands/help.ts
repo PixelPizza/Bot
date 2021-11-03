2ace41eaf1ea4bc3169635502b3ae62c68389fad
Jaron <jaron_zijlstra@outlook.com>
2021-11-03 21:16:28 +0100

    feat: added help command

---
+++ src/commands/help.ts
@@ -0,0 +1,44 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Args, Command, CommandOptions } from "@sapphire/framework";
+import { Message, MessageEmbed } from "discord.js";
+
+@ApplyOptions<CommandOptions>({
+	aliases: ["commands"],
+	description: "Shows a list of commands"
+})
+export class HelpCommand extends Command {
+	public async messageRun(message: Message, args: Args) {
+		const commands = this.container.client.stores.get("commands");
+		const command = args.finished ? null : commands.get(await args.pick("string"));
+
+		if (!command) {
+			return message.channel.send({
+				embeds: [
+					{
+						color: "BLUE",
+						title: "Commands",
+						description: commands.map((command) => `**${command.name}:** ${command.description}`).join("\n")
+					}
+				]
+			});
+		}
+
+		const embed = new MessageEmbed({
+			color: "BLUE",
+			fields: [
+				{
+					name: "Name",
+					value: command.name
+				}
+			]
+		});
+
+		command.aliases.length && embed.addField("Aliases", command.aliases.join(", "));
+		(command.detailedDescription || command.description) &&
+			embed.addField("Description", command.detailedDescription || command.description);
+
+		return message.channel.send({
+			embeds: [embed]
+		});
+	}
+}

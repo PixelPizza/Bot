49748442acf27e167fbc0b4fd4ee4ce2b88bcaa0
Jaron <jaron_zijlstra@outlook.com>
2021-12-29 16:26:09 +0100

    feat: add order command

---
+++ src/commands/order.ts
@@ -0,0 +1,69 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { ApplyOptions } from "@sapphire/decorators";
+import { ApplicationCommandRegistry, Command, CommandOptions, RegisterBehavior } from "@sapphire/framework";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import { randomInt } from "node:crypto";
+
+@ApplyOptions<CommandOptions>({
+	description: "Order some food",
+	requiredClientPermissions: ["CREATE_INSTANT_INVITE"]
+})
+export class OrderCommand extends Command {
+	public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+		registry.registerChatInputCommand(
+			new SlashCommandBuilder()
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption((input) =>
+					input.setName("order").setDescription("The order you want to place").setRequired(true)
+				),
+			{
+				behaviorWhenNotIdentical: RegisterBehavior.Overwrite
+			}
+		);
+	}
+
+	private async generateOrderID() {
+		const orders = this.container.stores.get("models").get("order");
+		let id: string;
+		do {
+			id = `00${randomInt(0, 999)}`.slice(-3);
+		} while (await orders.findByPk(id));
+		return id;
+	}
+
+	public override async chatInputRun(interaction: CommandInteraction) {
+		await interaction.deferReply({ ephemeral: true });
+
+		const order = interaction.options.getString("order", true);
+		const id = await this.generateOrderID();
+		const orders = this.container.stores.get("models").get("order");
+
+		await orders.create({
+			id,
+			order,
+			customer: interaction.user.id,
+			guild: interaction.guildId,
+			channel: interaction.channelId
+		});
+
+		await interaction.editReply({
+			embeds: [
+				new MessageEmbed({
+					color: "GREEN",
+					title: "Order Placed",
+					description: `Your order has been placed`,
+					fields: [
+						{
+							name: "Your order",
+							value: order
+						}
+					],
+					footer: {
+						text: `ID: ${id}`
+					}
+				})
+			]
+		});
+	}
+}

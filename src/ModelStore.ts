7c26ba16a2cebff7d8af29543e3198600445d9c5
Jaron <jaron_zijlstra@outlook.com>
2021-12-29 16:25:39 +0100

    feat(Models): add order model

--- src/ModelStore.ts
+++ src/ModelStore.ts
@@ -1,9 +1,19 @@
 import { Store } from "@sapphire/framework";
 import { ModelPiece } from "./pieces/Model";
+import type { OrderModel } from "./models/OrderModel";
 
 export class ModelStore extends Store<ModelPiece> {
 	public constructor() {
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 		super(ModelPiece as any, { name: "models" });
 	}
+
+	public override get<K extends keyof ModelStoreEntries>(key: K): ModelStoreEntries[K];
+	public override get(key: string) {
+		return super.get(key);
+	}
+}
+
+export interface ModelStoreEntries {
+	order: OrderModel;
 }
---
+++ src/models/OrderModel.ts
@@ -0,0 +1,122 @@
+import type { PieceContext } from "@sapphire/framework";
+import { DataTypes } from "sequelize";
+import { ModelPiece } from "../pieces/Model";
+
+export class OrderModel extends ModelPiece<{
+	id: {
+		type: DataTypes.StringDataType;
+		primaryKey: true;
+	};
+	customer: {
+		type: DataTypes.StringDataType;
+		allowNull: false;
+	};
+	guild: {
+		type: DataTypes.StringDataType;
+		allowNull: false;
+	};
+	channel: {
+		type: DataTypes.StringDataType;
+		allowNull: false;
+	};
+	chef: {
+		type: DataTypes.StringDataType;
+		defaultValue: null;
+	};
+	deliverer: {
+		type: DataTypes.StringDataType;
+		defaultValue: null;
+	};
+	image: {
+		type: DataTypes.StringDataType;
+		defaultValue: null;
+	};
+	status: {
+		type: DataTypes.EnumDataType<"unclaimed" | "claimed" | "cooking" | "cooked" | "delivered" | "deleted">;
+		allowNull: false;
+		defaultValue: "unclaimed";
+	};
+	order: {
+		type: DataTypes.StringDataType;
+		allowNull: false;
+	};
+	orderedAt: {
+		type: DataTypes.DateDataType;
+		allowNull: false;
+		defaultValue: typeof DataTypes.NOW;
+	};
+	cookedAt: {
+		type: DataTypes.DateDataType;
+		defaultValue: null;
+	};
+	deliveredAt: {
+		type: DataTypes.DateDataType;
+		defaultValue: null;
+	};
+	deliveryMethod: {
+		type: DataTypes.EnumDataType<"dm" | "bot" | "personal">;
+		defaultValue: null;
+	};
+}> {
+	public constructor(context: PieceContext) {
+		super(context, {
+			name: "order",
+			attributes: {
+				id: {
+					type: DataTypes.STRING(3),
+					primaryKey: true
+				},
+				customer: {
+					type: DataTypes.STRING(18),
+					allowNull: false
+				},
+				guild: {
+					type: DataTypes.STRING(18),
+					allowNull: false
+				},
+				channel: {
+					type: DataTypes.STRING(18),
+					allowNull: false
+				},
+				chef: {
+					type: DataTypes.STRING(18),
+					defaultValue: null
+				},
+				deliverer: {
+					type: DataTypes.STRING(18),
+					defaultValue: null
+				},
+				image: {
+					type: DataTypes.STRING(500),
+					defaultValue: null
+				},
+				status: {
+					type: DataTypes.ENUM("unclaimed", "claimed", "cooking", "cooked", "delivered", "deleted"),
+					allowNull: false,
+					defaultValue: "unclaimed"
+				},
+				order: {
+					type: DataTypes.STRING(2048),
+					allowNull: false
+				},
+				orderedAt: {
+					type: DataTypes.DATE(),
+					allowNull: false,
+					defaultValue: DataTypes.NOW
+				},
+				cookedAt: {
+					type: DataTypes.DATE(),
+					defaultValue: null
+				},
+				deliveredAt: {
+					type: DataTypes.DATE(),
+					defaultValue: null
+				},
+				deliveryMethod: {
+					type: DataTypes.ENUM("dm", "bot", "personal"),
+					defaultValue: null
+				}
+			}
+		});
+	}
+}

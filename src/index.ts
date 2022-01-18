459601a378005dbe967483da970df5eeec7be8d5
Jaron <jaron_zijlstra@outlook.com>
2022-01-18 20:30:59 +0100

    feat: set default registry bevavior

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import { Client } from "./Client";
 import { config } from "dotenv";
 import { ModelStore } from "./ModelStore";
 import parseEnv from "dotenv-parse-variables";
+import { ApplicationCommandRegistries, RegisterBehavior } from "@sapphire/framework";
 process.env = parseEnv(config().parsed!) as NodeJS.ProcessEnv;
 
 const client = new Client();
+
+ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.Overwrite);
+
 client.stores.register(new ModelStore());
 void client.login(process.env.TOKEN);

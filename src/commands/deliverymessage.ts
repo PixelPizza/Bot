84b3d9bf43e84df0aba5623f06b5e64bec90508e
Jaron <jaron_zijlstra@outlook.com>
2022-01-18 20:38:56 +0100

    feat: add delivery message command

---
+++ src/commands/deliverymessage.ts
@@ -0,0 +1,118 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import type { ApplicationCommandRegistry, CommandOptions } from "@sapphire/framework";
+import { codeBlock } from "@discordjs/builders";
+import { type CommandInteraction, type Message, MessageActionRow, MessageSelectMenu, type WebhookEditMessageOptions } from "discord.js";
+import { Command } from "../Command";
+import { Util } from "../Util";
+
+@ApplyOptions<CommandOptions>({
+    description: "Set your delivery message",
+    preconditions: ["DelivererOnly"]
+})
+export class DeliveryMessageCommand extends Command {
+    public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+        this.registerPrivateChatInputCommand(
+            registry,
+            this.defaultChatInputCommand
+                .addStringOption((input) => input.setName("message").setDescription("Your delivery message"))
+        );
+    }
+
+    private readonly requirements: ({
+        regex: string;
+        name: string;
+    } | string)[] = [
+        {
+            regex: `{${Util.makeUserRegex("chef")}}`,
+            name: "{chef}"
+        },
+        {
+            regex: `{${Util.makeUserRegex("customer")}}`,
+            name: "{customer}"
+        },
+        `{image}`,
+        `{invite}`
+    ];
+
+    public override async chatInputRun(interaction: CommandInteraction) {
+        await interaction.deferReply({ ephemeral: true });
+
+        const message = interaction.options.getString("message");
+        const [deliverer] = await this.container.stores.get("models").get("user").findOrCreate({
+            where: { id: interaction.user.id },
+            defaults: { id: interaction.user.id }
+        });
+
+        if (!message) {
+            const currentMessage = deliverer.getDataValue("deliveryMessage") ?? Util.getDefaults().deliveryMessage;
+            const replyOptions: WebhookEditMessageOptions = {
+                embeds: [{
+                    color: "BLUE",
+                    title: "Current Delivery Message",
+                    description: codeBlock(currentMessage)
+                }],
+                components: [
+                    new MessageActionRow({
+                        components: [
+                            new MessageSelectMenu({
+                                customId: "message",
+                                options: [
+                                    {
+                                        label: "Normal",
+                                        value: "normal",
+                                        default: true
+                                    },
+                                    {
+                                        label: "Colored",
+                                        value: "colored"
+                                    }
+                                ]
+                            })
+                        ]
+                    })
+                ]
+            };
+            const reply = await interaction.editReply(replyOptions) as Message;
+            reply.createMessageComponentCollector({componentType: "SELECT_MENU", filter: (selectMenu) => selectMenu.customId === "message"}).on("collect", async (interaction) => {
+                const normal = interaction.values[0] === "normal";
+                replyOptions.embeds![0].description = normal ? codeBlock(currentMessage) : codeBlock("ansi", currentMessage)
+                    .replace(/{(image|invite|orderID|order|guild|server|channel|chef|deliverer|customer|orderdate|cookdate|deliverydate)}/g, (_r, match) => `\x1b[0;34m{\x1b[0;32m${match}\x1b[0;34m}\x1b[0m`)
+                    .replace(/{(chef|deliverer|customer)(?:: *(tag|id|username|name|ping|mention))}/g, (_r, name, type) => `\x1b[0;34m{\x1b[0;32m${name}\x1b[0;36m:\x1b[0;33m${type}\x1b[0;34m}\x1b[0m`)
+                    .replace(/{(orderdate|cookdate|deliverydate)(?:: *(date|time|datetime))}/g, (_r, name, type) => `\x1b[0;34m{\x1b[0;32m${name}\x1b[0;36m:\x1b[0;33m${type}\x1b[0;34m}\x1b[0m`);
+                (replyOptions.components![0].components[0] as MessageSelectMenu).options[0].default = normal;
+                (replyOptions.components![0].components[0] as MessageSelectMenu).options[1].default = !normal;
+                await interaction.update(replyOptions);
+            });
+            return;
+        }
+
+        const missing: string[] = [];
+        this.requirements.forEach(requirement => {
+            const isString = typeof requirement === "string";
+            if (!(message.match(new RegExp(isString ? requirement : requirement.regex, "g")) || []).length)
+                missing.push(isString ? requirement : requirement.name);
+        });
+
+        if (missing.length) {
+            return interaction.editReply({
+                embeds: [{
+                    color: "RED",
+                    title: "Invalid delivery message",
+                    description: `Your delivery message is missing the following requirements: ${missing.join(", ")}`
+                }]
+            });
+        }
+
+        await deliverer.update({
+            deliveryMessage: message.replaceAll("\\n", "\n")
+        });
+
+        return interaction.editReply({
+            embeds: [{
+                color: "GREEN",
+                title: "Delivery Message Set",
+                description: "Your delivery message has been set"
+            }]
+        });
+    }
+}

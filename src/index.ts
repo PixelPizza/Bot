7ce0a797bd632ea8bf325f39f325ea0350281500
Jaron <60853956+JaronZ@users.noreply.github.com>
2022-03-29 21:57:48 +0200

    feat: add botlist plugin (#276)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+process.env.NODE_ENV ??= "development";
 import "@kaname-png/plugin-env/register";
 import "@sapphire/plugin-logger/register";
+import "@devtomio/plugin-botlist/register";
 import { Client } from "./lib/Client";
 import { ApplicationCommandRegistries, container, RegisterBehavior } from "@sapphire/framework";
 import { ModelManagerStore } from "./lib/stores/ModelManagerStore";
 import { join } from "path";
 import { WebhookManagerStore } from "./lib/stores/WebhookManagerStore";
-process.env.NODE_ENV ??= "development";
 
 async function main() {
     const client = new Client();

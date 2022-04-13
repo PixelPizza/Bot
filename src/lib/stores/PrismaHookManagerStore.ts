5a42011ba46ca2fabb1aabf6db9e7d7664c91ef6
Jaron <jaron_zijlstra@outlook.com>
2022-04-14 01:00:00 +0200

    fix: deepsource error

--- src/lib/stores/PrismaHookManagerStore.ts
+++ src/lib/stores/PrismaHookManagerStore.ts
@@ -6,11 +6,4 @@ export class PrismaHookManagerStore extends Store<PrismaHookManager<any>> {
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 		super(PrismaHookManager as any, { name: "hooks" });
 	}
-
-	public override get<K extends keyof PrismaHookManagerStoreEntries>(key: K): PrismaHookManagerStoreEntries[K];
-	public override get(key: string) {
-		return super.get(key);
-	}
 }
-
-export interface PrismaHookManagerStoreEntries {}

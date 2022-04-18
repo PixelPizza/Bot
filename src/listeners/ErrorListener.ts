d5c09297762ee49e2bde2dd298d41f88ce83da7a
Jaron <jaron_zijlstra@outlook.com>
2022-04-18 14:47:56 +0200

    feat: add some error listeners

---
+++ src/listeners/ErrorListener.ts
@@ -0,0 +1,12 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Events, Listener } from "@sapphire/framework";
+
+@ApplyOptions<Listener.Options>({
+    event: Events.Error
+})
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments
+export class ErrorListener extends Listener<typeof Events.Error> {
+    public run(error: Error): void {
+        this.container.logger.error(error);
+    }
+}

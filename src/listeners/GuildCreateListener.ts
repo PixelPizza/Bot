e5d9abcef714a87e858a332123def8968f4999b2
Jaron <jaron_zijlstra@outlook.com>
2022-04-21 14:55:52 +0000

    feat: add guild logs

---
+++ src/listeners/GuildCreateListener.ts
@@ -0,0 +1,13 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Events, Listener } from "@sapphire/framework";
+import type { Guild } from "discord.js";
+
+@ApplyOptions<Listener.Options>({
+    event: Events.GuildCreate
+})
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments
+export class GuildCreateListener extends Listener<typeof Events.GuildCreate> {
+    public run(guild: Guild): unknown {
+        return this.container.stores.get("webhooks").get("guild").sendGuild(guild, true);
+    }
+}

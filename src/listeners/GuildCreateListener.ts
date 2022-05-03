ef552b9150148b6af7879eada690388db9629f40
Jaron <jaron_zijlstra@outlook.com>
2022-05-03 21:26:49 +0200

    feat: add guild join message

--- src/listeners/GuildCreateListener.ts
+++ src/listeners/GuildCreateListener.ts
@@ -1,13 +1,22 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Events, Listener } from "@sapphire/framework";
-import type { Guild } from "discord.js";
+import { stripIndents } from "common-tags";
+import type { Guild, TextChannel } from "discord.js";
 
 @ApplyOptions<Listener.Options>({
 	event: Events.GuildCreate
 })
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments
 export class GuildCreateListener extends Listener<typeof Events.GuildCreate> {
-	public run(guild: Guild): unknown {
-		return this.container.stores.get("webhooks").get("guild").sendGuild(guild, true);
+	public async run(guild: Guild): Promise<unknown> {
+		await this.container.stores.get("webhooks").get("guild").sendGuild(guild, true);
+
+		const channel = guild.systemChannel ?? guild.channels.cache.find((channel) => channel.type === "GUILD_TEXT") as TextChannel;
+		return channel.send(stripIndents`
+			> Thank you for adding Pixel Pizza!
+			> Type / to see my commands.
+			> 
+			> To order food, use /order.
+		`);
 	}
 }

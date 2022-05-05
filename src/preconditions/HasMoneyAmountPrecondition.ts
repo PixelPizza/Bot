252ce48f284b9893fe3fb0979396c7d2e0b43857
Jaron <60853956+JaronZ@users.noreply.github.com>
2022-05-05 20:29:54 +0200

    feat: add give command (#302)
    * feat: add give command
    
    * Format code with standardjs and prettier
    
    * feat: add blacklist (#296)
    
    * feat: add blacklist to prisma
    
    * feat: add blacklist command
    
    * feat: add blacklist precondition
    
    * Format code with standardjs and prettier
    
    * feat: finish blacklist
    
    * Format code with standardjs and prettier
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    
    * feat: add menu command (#301)
    
    * feat: add menu command
    
    * Format code with standardjs and prettier
    
    * feat: add blacklist (#296)
    
    * feat: add blacklist to prisma
    
    * feat: add blacklist command
    
    * feat: add blacklist precondition
    
    * Format code with standardjs and prettier
    
    * feat: finish blacklist
    
    * Format code with standardjs and prettier
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    
    * feat: add menu command
    
    * Format code with standardjs and prettier
    
    * feat: add IdHints to menu command
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    
    * feat: add baltop command (#303)
    
    * feat: add baltop command
    
    * Format code with standardjs and prettier
    
    * feat: finish baltop command
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    
    * feat: add idHints to give command
    
    * feat: add give command
    
    * Format code with standardjs and prettier
    
    * feat: add idHints to give command
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ src/preconditions/HasMoneyAmountPrecondition.ts
@@ -0,0 +1,18 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Precondition, PreconditionOptions } from "@sapphire/framework";
+import type { CommandInteraction } from "discord.js";
+
+@ApplyOptions<PreconditionOptions>({
+	name: "HasMoneyAmount"
+})
+export class HasOrderPrecondition extends Precondition {
+	public override async chatInputRun(interaciton: CommandInteraction) {
+		const amount = interaciton.options.getInteger("amount");
+
+		if (!amount) return this.ok();
+
+		const { balance } = await this.container.findOrCreateUser(interaciton.user.id);
+
+		return balance >= amount ? this.ok() : this.error({ message: "You do not have enough money for this action" });
+	}
+}
---
+++ src/preconditions/UserExistsPrecondition.ts
@@ -0,0 +1,16 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Precondition, PreconditionOptions } from "@sapphire/framework";
+import type { CommandInteraction } from "discord.js";
+
+@ApplyOptions<PreconditionOptions>({
+	name: "UserExists"
+})
+export class HasOrderPrecondition extends Precondition {
+	public override async chatInputRun(interaciton: CommandInteraction) {
+		const user = interaciton.options.getUser("user");
+
+		if (!user || !(await this.container.prisma.user.findUnique({ where: { id: user.id } })))
+			return this.error({ message: "Unknown user" });
+		return this.ok();
+	}
+}

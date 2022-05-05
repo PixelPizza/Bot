887f431c2fbf51d1818f97898f4bb8de2d03dd33
Jaron <60853956+JaronZ@users.noreply.github.com>
2022-05-05 20:19:07 +0200

    feat: add menu command (#301)
    * feat: add menu command
    
    * Format code with standardjs and prettier
    
    * feat: add blacklist (#296)
    
    * feat: add blacklist to prisma
    
    * feat: add blacklist command
    
    * feat: add blacklist precondition
    
    * Format code with standardjs and prettier
    
    * feat: finish blacklist
    
    * Format code with standardjs and prettier
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    
    * feat: add menu command
    
    * Format code with standardjs and prettier
    
    * feat: add IdHints to menu command
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ src/commands/menu.ts
@@ -0,0 +1,90 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import type { ApplicationCommandRegistry } from "@sapphire/framework";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import { Command } from "../lib/commands/Command";
+
+@ApplyOptions<Command.Options>({
+	description: "Show the order menu"
+})
+export class MenuCommand extends Command {
+	private readonly items: {
+		name: string;
+		suffix?: string;
+		items: {
+			name: string;
+		}[];
+	}[] = [
+		{
+			name: "Pizza's",
+			suffix: " Pizza",
+			items: [
+				{
+					name: "Hawaï"
+				},
+				{
+					name: "Tonno"
+				},
+				{
+					name: "Pepperoni"
+				},
+				{
+					name: "Salami"
+				},
+				{
+					name: "Calzone"
+				},
+				{
+					name: "Margherita"
+				},
+				{
+					name: "BBQ Mixed Grill"
+				},
+				{
+					name: "Chicken Kebab"
+				},
+				{
+					name: "Shoarma"
+				},
+				{
+					name: "Cheese"
+				},
+				{
+					name: "Funghi"
+				},
+				{
+					name: "Chocolate"
+				}
+			]
+		},
+		{
+			name: "Burgers",
+			items: [
+				{
+					name: "Cheeseburger"
+				}
+			]
+		}
+	];
+
+	public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+		registry.registerChatInputCommand(this.defaultChatInputCommand, { idHints: ["971732311815114752"] });
+	}
+
+	public override chatInputRun(interaction: CommandInteraction) {
+		return interaction.reply({
+			embeds: [
+				new MessageEmbed()
+					.setColor("BLUE")
+					.setTitle("Menu")
+					.setDescription("Here are some examples of what to order")
+					.addFields(
+						...this.items.map((item) => ({
+							name: item.name,
+							value: item.items.map((subItem) => `${subItem.name}${item.suffix ?? ""}`).join("\n")
+						}))
+					)
+			],
+			ephemeral: true
+		});
+	}
+}

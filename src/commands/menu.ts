6f92c7c0e6863556410f63e19e9c8ac201e886d7
Jaron <jaron_zijlstra@outlook.com>
2022-05-05 20:43:35 +0200

    feat: add cooldowns for new commands

--- src/commands/menu.ts
+++ src/commands/menu.ts
@@ -1,10 +1,12 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import type { ApplicationCommandRegistry } from "@sapphire/framework";
+import { Time } from "@sapphire/time-utilities";
 import { CommandInteraction, MessageEmbed } from "discord.js";
 import { Command } from "../lib/commands/Command";
 
 @ApplyOptions<Command.Options>({
-	description: "Show the order menu"
+	description: "Show the order menu",
+	cooldownDelay: Time.Minute * 5
 })
 export class MenuCommand extends Command {
 	private readonly items: {

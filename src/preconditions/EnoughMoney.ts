4f64d3965b0a7d5782053deaa997726abc81c06b
Jaron <jaron_zijlstra@outlook.com>
2022-07-21 12:07:13 +0200

    fix: spelling mistake in EnoughMoney

--- src/preconditions/EnoughMoney.ts
+++ src/preconditions/EnoughMoney.ts
@@ -3,8 +3,8 @@ import { stripIndents } from "common-tags";
 import type { CommandInteraction } from "discord.js";
 
 export class EnoughMoneyPrecondition extends Precondition {
-	public override async chatInputRun(interaciton: CommandInteraction) {
-		const { balance } = await this.container.stores.get("models").get("user").findOrCreate(interaciton.user.id);
+	public override async chatInputRun(interaction: CommandInteraction) {
+		const { balance } = await this.container.stores.get("models").get("user").findOrCreate(interaction.user.id);
 
 		return balance >= this.container.env.integer("ORDER_PRICE")
 			? this.ok()
